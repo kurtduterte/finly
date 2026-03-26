2f6981c6aa2dfa7dff3b357884b311ee0f013df2
Kurt <keduterte@gmail.com>
2026-03-26 15:58:01 +0800

    restructured

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:finly/ui/feature/home/widgets/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: Text('Hello world'),
     );
   }
 }
--- lib/ui/feature/home/view_models/home_viewmodel.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class HomeViewModel extends ChangeNotifier {
-  bool get isLoading => false;
-}
