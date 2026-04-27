82f437157a18eab4b67e38579e6e11478ebc2704
Kurt <keduterte@gmail.com>
2026-04-27 16:18:04 +0800

    feat: add formatPeso currency utility

---
+++ lib/core/utils/currency_format.dart
@@ -0,0 +1,12 @@
+String formatPeso(int centavos) {
+  final amount = centavos / 100;
+  final parts = amount.toStringAsFixed(2).split('.');
+  final whole = parts[0];
+  final dec = parts[1];
+  final buf = StringBuffer();
+  for (var i = 0; i < whole.length; i++) {
+    if (i > 0 && (whole.length - i) % 3 == 0) buf.write(',');
+    buf.write(whole[i]);
+  }
+  return '₱$buf.$dec';
+}
